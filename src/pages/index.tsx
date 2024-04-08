70b0568e72fc33c34b0beb198477aa9b15b2b1b4
komeponpon <ricemountain1030@gmail.com>
2024-04-08 15:23:50 +0900

    投票モード変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,10 @@ const VotingApp = () => {
       setStalinsChoice(choice);
       setShowResults(true);
     } else if (mode === 'nash'){
+      const majority = getMajority();
       setVoteCount((prevCount) => ({
-        yes: prevCount.yes + (choice === 'はい' ? 1 : 0),
-        no: prevCount.no + (choice === 'いいえ' ? 1 : 0 ),
+        yes: majority === 'はい' ? prevCount.yes + 1 : prevCount.yes,
+        no: majority === 'いいえ' ? prevCount.no + 1 : prevCount.no,
       }));
       setShowResults(true);
     } else {
@@ -42,27 +43,38 @@ const VotingApp = () => {
         投票モード:
         <select value={mode} onChange={(e) => setMode(e.target.value)}>
           <option value="normal">通常</option>
-          <option value="stalin">ヨシフの選択</option>
-          <option value="nash">ナッシュ均衡</option>
+          <option value="stalin">スターリン</option>
+          <option value="nash">サイレントマジョリティ</option>
         </select>
       </div>
-      {!showResults?(
+      {!showResults ? (
         <div>
-          <button onClick={() => castVote('はい')}>はい</button>
-          <button onClick={() => castVote('いいえ')}>いいえ</button>
+          {mode === 'nash' ? (
+            <button onClick={castVote}>投票する</button>
+          ) : (
+            <>
+              <button onClick={() => castVote('はい')}>はい</button>
+              <button onClick={() => castVote('いいえ')}>いいえ</button>
+            </>
+          )}
         </div>
-        ) : (
-          <div>
-            <h2>結果</h2>
-            {mode === 'stalin' && <p>スターリンの選択: {stalinsChoice}</p>}
-            {mode === 'nash' && <p>サイレントマジョリティ: {getMajority()}</p>}
-            {mode === 'normal' && (
-              <p>
-                はい: {voteCount.yes}, いいえ: {voteCount.no}
-              </p>
-            )}
-            <button onClick={reset}>もう一度投票する</button>
-          </div>
+      ) : (
+        <div>
+          <h2>結果</h2>
+          {mode === 'stalin' && <p>スターリンの選択: {stalinsChoice}</p>}
+          {mode === 'nash' && (
+            <p>
+              サイレントマジョリティ: {getMajority()}に投票が加算されました <br />
+              はい: {voteCount.yes}, いいえ: {voteCount.no}
+            </p>
+          )}
+          {mode === 'normal' && (
+            <p>
+              はい: {voteCount.yes}, いいえ: {voteCount.no}
+            </p>
+          )}
+          <button onClick={reset}>もう一度投票する</button>
+        </div>
       )}
     </div>
   );
